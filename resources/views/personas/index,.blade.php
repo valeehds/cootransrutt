0156f329b59b2c2da9831299cb4bafc141dd533a
lauuthv <laurapedrazav145@gmail.com>
2023-08-16 15:12:21 -0500

    cootransrut

---
+++ resources/views/personas/index,.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.panel')
+
+@section('content')
+
+      <div class="card shadow">
+        <div class="card-header border-0">
+          <div class="row align-items-center">
+            <div class="col">
+              <h3 class="mb-0">Personas</h3>
+            </div>
+      <footer class="footer">
+        <div class="row align-items-center justify-content-xl-between">
+          <div class="col-xl-6">
+            <div class="copyright text-center text-xl-left text-muted">
+              &copy; 2023 <a href="#" class="font-weight-bold ml-1">{{ config('app.name')}}</a>
+            </div>
+          </div>
+          <div class="col-xl-6">
+            <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+              
+              <li class="nav-item">
+                <a href="#" class="nav-link">¿Quiénes somos?</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//1. PERSONAS
+
+//2.NÓMINAS
+
+//3. VEHICULOS
+
+//4. MANTENIMIENTOS
+
+//5. RUTAS
+
+//6. RECORRIDOS
+
+//7. ROTACIONES
+
+//8. DESPACHOS
